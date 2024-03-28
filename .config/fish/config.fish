ff9c912a2861de7ff5f568126897cbe1a66e7ee2
Serpent7776 <Serpent7776@gmail.com>
2024-03-28 22:37:54 +0100

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,33 @@
+if status is-interactive
+    # One of these is alt+backspace.
+    bind --mode insert \e\x7f backward-kill-word
+    bind --mode insert \e\b backward-kill-word
+
+    bind --mode insert \ca beginning-of-line
+    bind --mode insert \ce end-of-line
+
+    bind --mode insert \cf forward-char
+    bind --mode insert \cb backward-char
+
+    bind --mode insert \ct transpose-chars
+    bind --mode insert \et transpose-words
+
+    bind --mode insert \cp up-or-search
+    bind --mode insert \cn down-or-search
+
+    bind --mode insert \cr history-pager
+
+    alias poly 'rlwrap poly'
+    alias ocaml 'rlwrap ocaml'
+    alias tclsh 'rlwrap tclsh'
+    alias yazi '/opt/yazi-x86_64-unknown-linux-gnu/yazi'
+    alias R 'R --no-save'
+
+    . ~/data/git/cg/activate.fish
+
+    set -gx PAGER "less -R"
+    set CDPATH ~/data/git
+    set fish_help_browser midori
+
+    fish_add_path ~/.cargo/bin/
+end
